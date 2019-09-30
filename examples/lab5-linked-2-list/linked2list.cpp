9f311d5a384ec698e8b26c2bcc445db1faa1b3c7
Oleksii Vodka <oleksii.vodka@gmail.com>
2019-09-30 10:22:53 +0300

    lab5

---
+++ examples/lab5-linked-2-list/linked2list.cpp
@@ -0,0 +1,6 @@
+#include "linked2list.h"
+
+linked2list::linked2list()
+{
+
+}
---
+++ examples/lab5-linked-2-list/linked2list.h
@@ -0,0 +1,11 @@
+#ifndef LINKED2LIST_H
+#define LINKED2LIST_H
+
+
+class linked2list
+{
+public:
+    linked2list();
+};
+
+#endif // LINKED2LIST_H
